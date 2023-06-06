c26e2fe37accecbe514b7c8fd7cd79756002a5dc
TatevZaqaryan <tatevzaqaryan.1998@gmail.com>
2023-06-06 12:54:58 +0400

    client server , socket.io

---
+++ src/components/JoinBlock.jsx
@@ -0,0 +1,11 @@
+const JoinBlock = () => {
+  return (
+    <div className="join-block">
+      <input type="text" placeholder="Room ID" />
+      <input type="text" placeholder="Name" />
+      <button className="btn btn-success">Join\</button>
+    </div>
+  );
+};
+
+export default JoinBlock;
